9a1ebb44a9386f35c51d3c3a7dd7fa931131c545
stoplight@sip.tg <av@sip.tg>
2023-02-12 02:45:41 +0100

    New ways for creating a session
    Committed by siptg-bot on behalf of stoplight@sip.tg

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE0MjE5NA/spectral.js?branch_slug=main&token=0a47b55f-d0cd-4557-84ae-449b84ef9916";
+export default { extends: ruleset };
